6219f5739c4878653668b5b4e7be474ced4e00c8
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-10-02 20:39:55 -0500

    finished searching.py

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,18 @@
 # TO-DO: Implement a recursive implementation of binary search
 def binary_search(arr, target, start, end):
     # Your code here
-    pass
+    if start > end:
+        return -1
+    else:
+        middle = (start + end) // 2
+        guess = arr[middle]
+
+        if guess == target:
+            return middle
+        if guess < target:
+            return binary_search(arr, target, (middle + 1), end)
+        else:
+            return binary_search(arr, target, start, (middle - 1))
 
 
 # STRETCH: implement an order-agnostic binary search
